e8f0ae8205b06e5b47d56ca501a679a25932c9e3
Mike Nye <mike.nye@gmail.com>
2020-11-24 17:50:39 +0800

    add platforms & tag with version specific

--- buildx.sh
+++ buildx.sh
@@ -2,10 +2,18 @@
 
 REPO=mikenye
 IMAGE=striparr
+PLATFORMS="linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
 
 docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 docker buildx use homecluster
 
 # Build & push latest
-docker buildx build -t ${REPO}/${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+docker buildx build -t "${REPO}/${IMAGE}:latest" --compress --push --platform "${PLATFORMS}" .
+
+# Get version
+COMMITHASH="$(git log | head -1 | cut -d ' ' -f 2)"
+VERSION="${COMMITHASH:0:7}"
+
+# Build & push version specific
+docker buildx build -t "${REPO}/${IMAGE}:${VERSION}" --compress --push --platform "${PLATFORMS}" .
